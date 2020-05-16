5166891d1f9f13c222f469798628f39a6eb924b3
paulmburu08 <paulmburu08@gmail.com>
2020-05-17 02:06:41 +0300

    add changes

--- src/app/services/repo-search.service.ts
+++ src/app/services/repo-search.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { RepoSearch } from '../modules/repo-search';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoSearchService {
 
-  repoSearchUrl = `${environment.repoUrl}`
-  userSearchUrl = `${environment.userUrl}` 
-  API_KEY = `${environment.apiKey}`
-  
-  constructor() { }
+  repoSearchUrl = `${environment.repoUrl}`;
+  userSearchUrl = `${environment.userUrl}`;
+  API_KEY = `${environment.apiKey}`;
+  repoSearch : RepoSearch;
+
+  constructor(http: HttpClient) { }
 }
